07406d84ccbd189faeac19d19598077539483b9d
Mike Ellery <mike.ellery@primephotonics.com>
2022-06-24 09:35:15 -0400

    add factors and is_prime functions

--- euler_common.py
+++ euler_common.py
@@ -1,3 +1,6 @@
+from math import sqrt
+from functools import reduce
+
 def eratosthenes(n):
     m = n+1
     numbers = [True] * m 
@@ -18,3 +21,22 @@ def gcd(a, b):
 
 def lcm(a, b):
     return a*b/gcd(a, b)
+
+def factors(n):    
+    return set(reduce(list.__add__, 
+        ([i, n//i] for i in range(1, int(n**0.5) + 1) if n % i == 0)))
+
+def is_prime(n):
+    prime_flag = 0
+  
+    if(n > 1):
+        for i in range(2, int(sqrt(n)) + 1):
+            if (n % i == 0):
+                prime_flag = 1
+                break
+        if (prime_flag == 0):
+            return True
+        else:
+            return False
+    else:
+        return False
