2637820c32fffa84733cde3a6fdb104b50f51a6e
Mike Ellery <mellery@gmail.com>
2022-08-02 12:48:23 -0400

    add problem28

---
+++ problem28.py
@@ -0,0 +1,25 @@
+from cv2 import cornerSubPix
+
+
+def problem28(size):
+    edge = 1
+    corners = 0
+    diagonals = [1]
+    i = 3
+    edge = 2
+    while (i <= size*size):
+        print("corner at ",i,"edge len is ",edge)
+        diagonals.append(i)
+        corners = corners + 1
+        if corners == 4:
+            edge = edge + 2
+            corners = 0
+        i = i + edge
+        
+    #diagonals.append(size*size)
+    print(i,edge)
+    print(diagonals)
+    return(sum(diagonals))
+ 
+#print(problem28(5))
+print(problem28(1001))
