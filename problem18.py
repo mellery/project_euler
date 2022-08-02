f5a883fa7ad4f7cc05f503627f984cc3b2a4288c
Mike Ellery <mellery@gmail.com>
2022-08-02 12:48:21 -0400

    add problems 18 and 67

---
+++ problem18.py
@@ -0,0 +1,26 @@
+def problem18(input):
+    ans = 0
+
+    file1 = open(input, 'r')
+    lines = file1.readlines()
+
+    rows = []
+    
+    for l in lines:
+        rows.append(l.split())
+
+    for i in range(0,len(rows)):
+        rows[i] = [int(x) for x in rows[i]]
+    
+    while len(rows) > 1:
+        for x in range(0,len(rows[-2])):
+            if (rows[-1][x] > rows[-1][x+1]):
+                rows[-2][x] = rows[-2][x] + rows[-1][x]
+            else:
+                rows[-2][x] = rows[-2][x] + rows[-1][x+1]
+
+        rows.pop()
+
+    return rows[0][0]
+
+print(problem18("input/problem18.txt"))
