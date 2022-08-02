f7a3f45079679343b839effd8f94d3151a2d921e
Mike Ellery <mellery@gmail.com>
2022-08-02 12:48:20 -0400

    add problem13

---
+++ problem13.py
@@ -0,0 +1,12 @@
+def problem13(input):
+    ans = 0
+
+    file1 = open(input, 'r')
+    lines = file1.readlines()
+
+    values = []
+    for l in lines:
+        values.append(int(l.strip()))
+    return(str(sum(values))[0:10])
+
+print(problem13("input/problem13.txt"))
